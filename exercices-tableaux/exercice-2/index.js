b6a787e60d6b1e52b1b28f37df57a8dcd79ada9f
smimoon <simonlaroche@orange.fr>
2026-01-07 09:21:18 +0000

    Save after finishing the exercise

--- exercices-tableaux/exercice-2/index.js
+++ exercices-tableaux/exercice-2/index.js
@@ -1,2 +1,14 @@
 let animals_even = ["Dog", "Cat", "Spider"];
 let animals_odd = ["Bat", "Snake", "Bear"];
+
+for(let i=0; i<animals_even.length; i++) {
+    if(i%2===0) {
+        console.log(animals_even[i]);
+    }
+}
+
+for(let j=0; j<animals_odd.length; j++) {
+    if(j%2!==0) {
+        console.log(animals_odd[j]);
+    }
+}
