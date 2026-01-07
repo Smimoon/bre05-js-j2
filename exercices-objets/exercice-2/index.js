596547f07c051d03c7e60ae29247f259eab16025
smimoon <simonlaroche@orange.fr>
2026-01-07 13:30:31 +0000

    Save end of exercise

--- exercices-objets/exercice-2/index.js
+++ exercices-objets/exercice-2/index.js
@@ -4,3 +4,12 @@ let student = {
 	grades : [12, 14, 8],
 	average : 0
 };
+let sum= 0;
+let div= 0;
+
+for(let i=0; i<student.grades.length; i++) {
+    sum = sum + student.grades[i];
+    div++;
+}
+student.average = sum/div;
+console.log(student.average);
