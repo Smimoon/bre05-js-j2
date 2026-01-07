27c049ca25d009c5d6187a0a856459f03ae8c911
smimoon <simonlaroche@orange.fr>
2026-01-07 10:53:42 +0000

    Save after upgrading index.js code to make it more generic

--- exercices-tableaux/exercice-4/index.js
+++ exercices-tableaux/exercice-4/index.js
@@ -1,12 +1,14 @@
 let grades = [[20, 10], [15, 20], [13, 18]];
 let sum = 0;
+let div = 0;
 let average = 0;
 
 for(let i=0; i<grades.length; i++) {
    for(let j=0; j<grades[i].length; j++) {
        sum = sum + grades[i][j];
+       div = div+1;
    }
 }
-console.log(sum)
-average = sum / 6
-console.log(average)
+console.log(sum);
+average = sum / div;
+console.log(average);
