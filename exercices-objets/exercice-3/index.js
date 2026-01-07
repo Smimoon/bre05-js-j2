89824013b9fc13e4fc53721adfd8aef2f9da8055
smimoon <simonlaroche@orange.fr>
2026-01-07 13:33:59 +0000

    Save beginning of exercise

---
+++ exercices-objets/exercice-3/index.js
@@ -0,0 +1,17 @@
+let students = [
+	{
+		name : "Steven",
+		age : 18,
+		average : 10
+	},
+	{
+		name : "Jessica",
+		age : 20,
+		average : 13
+	},
+	{
+		name : "Charlie",
+		age : 17,
+		average : 12
+	},
+];
