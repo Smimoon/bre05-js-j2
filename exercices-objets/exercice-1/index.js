59451f12e02d29b40349369f20e547adf8e7e04e
smimoon <simonlaroche@orange.fr>
2026-01-07 13:09:46 +0000

    Save end exercise

---
+++ exercices-objets/exercice-1/index.js
@@ -0,0 +1,7 @@
+let Student = {
+    name : "Edmond",
+    age : "22",
+    average : "16,2",
+};
+
+console.log(`Mon nom est ${Student.name}, j'ai ${Student.age} ans et ma moyenne est de ${Student.average}`);
